045a6296d447d5b82bb4508729ab1335439652d8
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-06-18 23:45:39 -0700

    Audio Context test + tsconfig update

--- public/main.js
+++ public/main.js
@@ -1,4 +1,20 @@
 "use strict";
-console.log('hello there');
-async function hello() {
+const AUDIO_FILE = new Audio('./assets/audio/voyager.mp3');
+console.log('test');
+console.log('test');
+const audioCtx = new window.AudioContext;
+const myScript = document.querySelector('script');
+const channels = 2;
+const frameCount = audioCtx.sampleRate * 2.0;
+const myArrayBuffer = audioCtx.createBuffer(2, frameCount, audioCtx.sampleRate);
+function whiteNoise() {
+    for (let channel = 0; channel < channels; channel++) {
+        const nowBuffering = myArrayBuffer;
+        console.log('test');
+    }
 }
+function init() {
+    const button = document.querySelector('button');
+    button === null || button === void 0 ? void 0 : button.addEventListener('click', whiteNoise);
+}
+document.addEventListener('DOMContentLoaded', init);
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,24 @@
-const AUDIO_FILE = new Audio('../assets/audio/voyager.mp3');
+const AUDIO_FILE = new Audio('./assets/audio/voyager.mp3');
+console.log('test');
+console.log('test');
+const audioCtx = new window.AudioContext;
+const myScript = document.querySelector('script');
 
+const channels = 2;
+const frameCount = audioCtx.sampleRate * 2.0;
+const myArrayBuffer = audioCtx.createBuffer(2, frameCount, audioCtx.sampleRate);
+
+function whiteNoise() :void {
+    for (let channel = 0; channel < channels; channel++) {
+        const nowBuffering = myArrayBuffer;
+        console.log('test');
+
+    }
+}
+
+function init() :void {
+    const button = document.querySelector('button');
+    button?.addEventListener('click', whiteNoise );
+}
+
+document.addEventListener('DOMContentLoaded', init);
