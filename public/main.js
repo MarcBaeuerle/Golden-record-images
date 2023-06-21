015184f15409e7ebb13a4e01d13b49e15dcc769b
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-06-20 22:44:18 -0700

    scruffed planets

--- public/main.js
+++ public/main.js
@@ -1,20 +1,27 @@
 "use strict";
-const AUDIO_FILE = new Audio('./assets/audio/voyager.mp3');
-console.log('test');
-console.log('test');
-const audioCtx = new window.AudioContext;
-const myScript = document.querySelector('script');
+//const AUDIO_FILE = new Audio('./assets/audio/voyager.mp3');
 const channels = 2;
-const frameCount = audioCtx.sampleRate * 2.0;
-const myArrayBuffer = audioCtx.createBuffer(2, frameCount, audioCtx.sampleRate);
-function whiteNoise() {
-    for (let channel = 0; channel < channels; channel++) {
-        const nowBuffering = myArrayBuffer;
-        console.log('test');
-    }
+const ctx = new AudioContext();
+const frameCount = ctx.sampleRate * 2.0;
+const myArrayBuffer = ctx.createBuffer(channels, frameCount, ctx.sampleRate);
+let audio;
+fetch(`./src/assets/audio/voyager.mp3`)
+    .then(data => data.arrayBuffer())
+    .then(ArrayBuffer => ctx.decodeAudioData(ArrayBuffer))
+    .then(decodedAudio => {
+    audio = decodedAudio;
+    console.log(typeof audio);
+    console.log(`audo decoded`);
+});
+function playBack() {
+    console.log(`Entered playBack()`);
+    const playSound = ctx.createBufferSource();
+    playSound.buffer = audio;
+    playSound.connect(ctx.destination);
+    playSound.start(ctx.currentTime);
 }
 function init() {
     const button = document.querySelector('button');
-    button === null || button === void 0 ? void 0 : button.addEventListener('click', whiteNoise);
+    button === null || button === void 0 ? void 0 : button.addEventListener('click', playBack);
 }
 document.addEventListener('DOMContentLoaded', init);
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,43 @@
-const AUDIO_FILE = new Audio('./assets/audio/voyager.mp3');
-console.log('test');
-console.log('test');
-const audioCtx = new window.AudioContext;
-const myScript = document.querySelector('script');
-
+//const AUDIO_FILE = new Audio('./assets/audio/voyager.mp3');
 const channels = 2;
-const frameCount = audioCtx.sampleRate * 2.0;
-const myArrayBuffer = audioCtx.createBuffer(2, frameCount, audioCtx.sampleRate);
 
-function whiteNoise() :void {
-    for (let channel = 0; channel < channels; channel++) {
-        const nowBuffering = myArrayBuffer;
-        console.log('test');
+const audioCtx = new window.AudioContext();
+let source: object;
 
-    }
+const ctx = new AudioContext();
+const frameCount = ctx.sampleRate * 2.0; 
+const myArrayBuffer = ctx.createBuffer(
+    channels, 
+    frameCount,
+    ctx.sampleRate
+);
+
+let audio: object;
+
+function getData(): void{
+    source = audioCtx.createBufferSource();
+    
+    fetch(`./src/assets/audio/voyager.mp3`)
+    .then(data => data.arrayBuffer())
+    .then(ArrayBuffer => audioCtx.decodeAudioData(ArrayBuffer))
+    .then(decodedAudio => {
+        audio = decodedAudio;
+        console.log(typeof audio);
+        console.log(`audo decoded`);
+    })
 }
 
+
+function playBack():void {
+    console.log(`Entered playBack()`);
+    const playSound = ctx.createBufferSource();
+    //playSound.buffer = audio;
+    playSound.connect(ctx.destination);
+    playSound.start(ctx.currentTime);
+}
 function init() :void {
     const button = document.querySelector('button');
-    button?.addEventListener('click', whiteNoise );
+    button?.addEventListener('click', playBack);
 }
 
 document.addEventListener('DOMContentLoaded', init);
