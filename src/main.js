124d6f2d9fe2a83670295ecab7fd2c8d18ec3e99
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-06-24 23:18:55 -0700

    audio visualizer kinda works

--- src/main.js
+++ src/main.js
@@ -1,26 +1,21 @@
 
 //const AUDIO_FILE = new Audio('./assets/audio/voyager.mp3');
 const CHANNELS = 2;
-const HERTZ = 44100;
 const CANVAS_HEIGHT = 390;
 const CANVAS_WIDTH = 520;
-
+const SAMPLE_RATE = 44100;
 
 const audioContext = new AudioContext({
-    sampleRate: 44100,
+    sampleRate: SAMPLE_RATE,
 });
 const analyzer = audioContext.createAnalyser();
 
 let dom = {};
+let context;
 let audio;
 let leftChannelData;
 let rightChannelData;
 
-const testBuffer = audioContext.createBuffer(
-    CHANNELS,
-    HERTZ * 1,
-    HERTZ
-)
 
 function getAudio(){
     fetch(`./src/assets/audio/voyager.mp3`)
@@ -30,10 +25,12 @@ function getAudio(){
             leftChannelData = decodedAudio.getChannelData(0);
             rightChannelData = decodedAudio.getChannelData(1);
             console.log(`Sample rate: ${decodedAudio.sampleRate}`)
-            let leftAmplitudeData = Array.from(leftChannelData, sample => Math.abs(sample));
+            console.log(`Length ${leftChannelData.length / decodedAudio.sampleRate}`);
+            let leftAmplitudeData = Array.from(leftChannelData, sample => sample);
             let rightAmplitudeData = Array.from(rightChannelData, sample => Math.abs(sample));
             //printAudioBuffer(leftAmplitudeData, rightAmplitudeData);
-            printAudioBufferTimeFrame(leftAmplitudeData, 900, 901);
+            printAudioBufferTimeFrame(leftAmplitudeData, 0, 1);
+            visualizeAudio(decodedAudio);
         })
         .catch(error => {
             console.log('Error fetching or decoding audio: ', error);
@@ -51,26 +48,72 @@ function printAudioBuffer(buf1, buf2) {
 }
 
 function printAudioBufferTimeFrame(buf, startTime, endTime) {
-    let startSample = Math.floor(startTime * audioContext.samplerate);
-    let endSample = Math.floor(endTime * audioContext.samplerate);
-
-
+    let startSample = Math.floor(startTime * SAMPLE_RATE);
+    let endSample = Math.floor(endTime * SAMPLE_RATE);
+    console.log(`between: ${startTime} and ${endTime}`);
+    console.log(`start: ${startSample} + end: ${endSample}`);
+ 
+    let total = 0
+    let max = 0;
     for (let i = startSample; i < endSample; i++) {
-        console.log(`buf: ${buf[i]}`);
+        //console.log(`${i} buf: ${buf[i]}`);
+        if (buf[i] > max) {
+            max = buf[i];
+        }
+        //console.log(`${i} diff: ${buf[i] - buf[i-1]}`)
+        total += 1;
     }
-
-
+    console.log(total);
+    console.log(max);
 }
 
-function audioTest(var1) {
-    console.log(var1);
-}
+function visualizeAudio(audioBuffer) {
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
 
-function populateCanvas() {
-    for (let i = 0; i < CANVAS_HEIGHT; i++) {
+    const analyser = audioContext.createAnalyser();
+    source.connect(analyser);
+    //analyser.connect(audioContext.destination);
 
+    //analyser setup
+    analyser.fftSize = 512;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    function renderFrame() {
+        requestAnimationFrame(renderFrame);
+
+        analyser.getByteTimeDomainData(dataArray);
+
+        context.clearRect(0, 0, dom.canvas.width, dom.canvas.height);
+
+        const bufferLength = dataArray.length;
+        const sliceWidth = dom.canvas.width / bufferLength;
+        let x = 0;
+
+        context.beginPath();
+        context.lineWidth = 2;
+        context.strokeStyle = 'rgb(0,0,0)';
+
+        for (let i = 0; i < bufferLength; i++) {
+            const sample = dataArray[i] / 128.0;
+            const y = (sample * dom.canvas.height) / 2;
+
+            if (i === 0) {
+                context.moveTo(x, y);
+            } else {
+                context.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+        }
+
+        context.lineTo(dom.canvas.width, dom.canvas.height / 2);
+        context.stroke();
     }
-    
+
+    source.start(0);
+    renderFrame();
 }
 
 function init() {
@@ -78,7 +121,10 @@ function init() {
         button: document.querySelector('button'),
         leftCanvas: document.querySelector('.left-canvas'),
         rightCanvas: document.querySelector('.right-canvas'),
+        canvas: document.querySelector('#waveformCanvas'),
 	};
+    
+    context = dom.canvas.getContext('2d');
 
     dom.button.addEventListener('click', () => {
         console.log(`getting data 1 sec`);
