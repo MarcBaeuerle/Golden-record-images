e1a6ab72f804af38617e6b199dc27c6c96f3b3e4
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-06-29 18:36:25 -0700

    Images aligned properly, all left channel works

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 // 512 columns make up one image
 // each column is 8.3ms long
 //const AUDIO_FILE = new Audio('./assets/audio/voyager.mp3');
 const CHANNELS = 2;
-const CANVAS_HEIGHT = 364;
-const CANVAS_WIDTH = 512;
+let CANVAS_HEIGHT;
+let CANVAS_WIDTH;
 const SAMPLE_RATE = 44100;
 const red = 0;
 const grn = 1;
@@ -17,7 +20,18 @@ const IMAGE_DATA = {
         amplitudeData: null,
         pointer: 0,
         timeStamps: [
-            691496, 956716, 1227516, 1488461, 1745996, 
+            691479, 956699, 1226764, 1488444, 1746346, 2003886, 2253818,            //0
+            2495501, 2738833, 2981916, 3295249, 3549540, 3812059, 4072272,          //7
+            4322136, 4575677, 4822611, 5060736, 5312170, 5569428, 5809626,          //14
+            6060410, 6312643, 6581734, 6837650, 7088915, 7340062, 7597294,          //21
+            7843816, 8098952, 8356665, 8598180, 8873049, 9132202, 9386246,          //28
+            9642641, 9897876, 10149529, 10422986, 10672601, 10931249, 11187243,     //35
+            11444253, 11702810, 11961521, 12220751, 12480495, 12724703, 12977799,   //42
+            13234000, 13492635, 13752796, 13998981, 14249093, 14495646, 14746329,   //49
+            14999878, 15249029, 15499590, 15755055, 15996882, 16247674, 16493176,   //56
+            16748591, 17006625, 17265784, 17511919, 17772949, 18015077, 18278128,   //63
+            18541817, 18838219, 19081581, 19338182, 19582170, 19829758, 20078770,   //70
+            20354900,                                                               //77
         ],
         colors: [
             bnw, bnw, bnw, bnw, bnw, bnw, bnw, red, grn, blu,   //0
@@ -85,6 +99,7 @@ let gotAudio = false;
 
 function getAudio(){
     fetch(`./src/assets/audio/voyager.mp3`)
+    //fetch(`./src/assets/audio/384kHzStereo.wav`)
         .then(data => data.arrayBuffer())
         .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
         .then(decodedAudio => {
@@ -98,6 +113,8 @@ function getAudio(){
             //printAudioBuffer(leftAmplitudeData, rightAmplitudeData);
             printAudioBufferTimeFrame(leftAmplitudeData, 0, 1);
             //visualizeAudio(decodedAudio);
+
+            gotAudio = true;
         })
         .catch(error => {
             console.log('Error fetching or decoding audio: ', error);
@@ -183,12 +200,20 @@ function visualizeAudio(audioBuffer) {
     renderFrame();
 }
 
+function findNextPeak(channel, position) {
+    const LOCAL_MIN = position + 730;
+    const LOCAL_MAX = position + 740;
 
-function findNextDrop(channel) {
-    let current = channel.pointer;
+    let newMax = 0;
+    let newPosition = position;
 
-    console.log(current);
-
+    for (let i = LOCAL_MIN; i < LOCAL_MAX; i++){
+        if (channel.amplitudeData[i] > newMax) {
+            newMax = channel.amplitudeData[i];
+            newPosition = i;
+        }
+    }
+    channel.pointer = newPosition;
 }
 
 function drawSingleLine(channel, offset, wIndex) {
@@ -214,7 +239,32 @@ function drawSingleLine(channel, offset, wIndex) {
         linePixelRow[3+i*4] = 255;
     }
 
-    context.putImageData(lineImageData, wIndex, 0);
+    setTimeout(() => {
+        context.putImageData(lineImageData, wIndex, 0);
+    },200);
+
+}
+
+
+async function displayImage(channel, index) {
+    setTimeout(() => {
+        if (!gotAudio) return;
+        channel.pointer = channel.timeStamps[index];
+        let oldPosition;
+
+        for (let i = 0; i < CANVAS_WIDTH; i++) {
+            drawSingleLine(IMAGE_DATA.left, IMAGE_DATA.left.pointer, i);
+
+            if (i % 2 === 0 && i!= 0) {
+                findNextPeak(IMAGE_DATA.left, oldPosition);
+            } else {
+                oldPosition = IMAGE_DATA.left.pointer;
+
+                IMAGE_DATA.left.pointer += CANVAS_HEIGHT;
+            }
+        }
+        console.log(IMAGE_DATA.left.pointer);
+    }, 1000);
 
 }
 
@@ -229,6 +279,11 @@ function init() {
         drawBtn: document.querySelector("#draw-circle"),
 	};
     getAudio();
+
+    CANVAS_WIDTH = dom.leftChannelImage.width;
+    console.log(CANVAS_WIDTH);
+    CANVAS_HEIGHT = dom.leftChannelImage.height;
+    console.log(CANVAS_HEIGHT);
     
     context = dom.canvas.getContext('2d');
 
@@ -237,40 +292,12 @@ function init() {
     });
 
     dom.drawBtn.addEventListener('click', () => {
-        if (!gotAudio) return;
-        IMAGE_DATA.left.pointer = IMAGE_DATA.left.timeStamps[0];
-        for (let i = 0; i < CANVAS_WIDTH; i++) {
-            drawSingleLine(IMAGE_DATA.left, IMAGE_DATA.left.pointer, i);
-            IMAGE_DATA.left.pointer += CANVAS_HEIGHT;
-            if (i % 2 === 0) {
-                IMAGE_DATA.left.pointer += 6;
-            }
+        for (let i = 0; i < 78; i++) {
+            displayImage(IMAGE_DATA.left, i);
         }
-        findNextDrop(IMAGE_DATA.left);
-        console.log(IMAGE_DATA.left.pointer);
     });
 
 }
 
 document.addEventListener('DOMContentLoaded', init);
-//const source = audioContext.createMediaStreamSource();
-//source.connect(analyzer);
-//analyzer.connect(distortion);
-
-
-//const channelData = buffer.getChannelData(0);
-//
-//for (let i = 0; i < buffer.length; i++) {
-//    channelData[i] = Math.random() * 2 - 1;
-//}
-//
-//const whiteNoiseSource = audioContext.createBufferSource()
-//whiteNoiseSource.buffer = buffer;
-//
-//const primaryGainControl = audioContext.createGain();
-//primaryGainControl.gain.setValueAtTime(0.05, 0);
-//
-//whiteNoiseSource.connect(primaryGainControl);
-//primaryGainControl.connect(audioContext.destination);
-
 
